cb3f6ad3d16cbd36575b2b5e41c86003113db74e
DaveWork19 <ddcy19@gmail.com>
2024-10-25 13:12:10 -0600

    Update frontend Oct25

--- frontend/src/nbaPages/massiveParlay.js
+++ frontend/src/nbaPages/massiveParlay.js
@@ -1,2 +1,4 @@
 export const massiveParlay = [
+'Atlanta Hawks,Charlotte Hornets,spread,1',
+'Milwaukee Bucks,Chicago Bulls,spread,1',
 ];
--- frontend/src/nbaPages/smallParlay.js
+++ frontend/src/nbaPages/smallParlay.js
@@ -1,5 +1,11 @@
 export const smallParlay = [
-'Dallas Mavericks,San Antonio Spurs,overUnder,1',
-'Oklahoma City Thunder,Denver Nuggets,overUnder,0',
-'Sacramento Kings,Minnesota Timberwolves,overUnder,1',
+'Philadelphia 76ers,Toronto Raptors,overUnder,1',
+'Orlando Magic,Brooklyn Nets,overUnder,1',
+'New York Knicks,Indiana Pacers,overUnder,1',
+'Atlanta Hawks,Charlotte Hornets,spread,1',
+'Detroit Pistons,Cleveland Cavaliers,spread,1',
+'Milwaukee Bucks,Chicago Bulls,spread,1',
+'Houston Rockets,Memphis Grizzlies,spread,1',
+'Utah Jazz,Golden State Warriors,overUnder,1',
+'New Orleans Pelicans,Portland Trail Blazers,overUnder,0',
 ];
